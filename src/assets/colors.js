6a240d309ba47f90bd2214a2001a56cab729af04
FrancoBorsella1 <fborsella1@gmail.com>
2023-09-16 18:27:54 -0300

    Se modificó la paleta de colores

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -1,8 +1,11 @@
 const COLORS = {
-    crema: "#FBFFF1",
-    celeste: "#B4C5E",
-    azul: "#3066BE",
-    gris: "3C3744"
+    terciario: "#FBFFF1", //Beige 60%
+    secundario: "#B4C5E", //Celeste 30%
+    primario: "#3066BE", //Azul 10%
+    inactivo:"#898989", //Gris claro. lineas, placeholder, texto inactivo
+    extra: "#C9CCC1", //Gris oscuro. Para generar contraste o arreglar: Footer
+    blanco: "#FFFFFF",
+    negro: "#000000"
 }
 
 export default COLORS;
