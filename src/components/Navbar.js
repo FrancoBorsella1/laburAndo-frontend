70fba2e6d4bdcaa54d4d34ed3b598898149b6cd4
cintiavalero <cintiavalero2001@gmail.com>
2023-09-16 22:07:20 -0300

    Prueba: se comentaron lineas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../assets/logos/navbar-logo.png";
 import home from "../assets/icons/icono-home.svg";
 import profile from "../assets/icons/icono-usuario.svg";
-import punto from "../assets/icons/icono-punto.svg";
+// import punto from "../assets/icons/icono-punto.svg";
 import "../styles/Navbar.css";
 
 
@@ -13,11 +13,11 @@ function Navbar() {
             <div className="navbar-options">
                 <div className="nav-option" id="option-home">
                     <img src={home} alt="home" className="icono-option"></img>
-                    <img src={punto} alt="punto" className="icono-punto"></img>
+                    {/* <img src={punto} alt="punto" className="icono-punto"></img> */}
                 </div>
                 <div className="nav-option" id="option-profile">
                     <img src={profile} alt="profile" className="icono-option"></img>
-                    <img src={punto} alt="punto" className="icono-punto"></img>
+                    {/* <img src={punto} alt="punto" className="icono-punto"></img> */}
                 </div>
             </div>          
             <a href="#cerrarsesion">Cerrar sesión</a> {/*Agregar el link hacia la página del login*/}
