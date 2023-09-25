edb48a60fe9fb25cac7ae16d379385f7df9d17f7
FrancoBorsella1 <fborsella1@gmail.com>
2023-09-25 14:58:44 -0300

    fix: Se corrigieron problemas de compatibilidad en la navegación

--- src/components/Publicacion.js
+++ src/components/Publicacion.js
@@ -3,17 +3,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpRightAndDownLeftFromCenter } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Publicacion.css";
 
-function Publicacion(props) {
+function Publicacion({ publicacion, onAmpliar }) {
     return(
         <article id="contenedor-publicacion">
             <div className="publicacion-header">
-                <p className="publicacion-titulo">{props.titulo}</p>
-                <p className="fecha">{props.fecha}</p>
+                <p className="publicacion-titulo">{publicacion.titulo}</p>
+                <p className="fecha">{publicacion.fecha}</p>
             </div>
             <div className="publicacion-content">
-                <p className="publicacion-servicio"><span>Servicio:</span> {props.servicio}</p>
-                <p className="publicacion-localidad"><span>Localidad:</span> {props.localidad}</p>
-                <FontAwesomeIcon icon={ faUpRightAndDownLeftFromCenter } id='publicacion-icono'/>
+                <p className="publicacion-servicio"><span>Servicio:</span> {publicacion.servicio}</p>
+                <p className="publicacion-localidad"><span>Localidad:</span> {publicacion.localidad}</p>
+                <FontAwesomeIcon icon={ faUpRightAndDownLeftFromCenter } id='publicacion-icono' onClick={() => onAmpliar(publicacion)}/>
             </div>
         </article> 
     );
