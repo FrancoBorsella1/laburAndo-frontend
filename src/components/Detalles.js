5fa5ce9c0cad434cb18caa16499e6145992ea2f4
cintiavalero <cintiavalero2001@gmail.com>
2023-09-25 02:51:58 -0300

    fix: Se ajustaron estilos que pisaban al componente AltaPublicacion

--- src/components/Detalles.js
+++ src/components/Detalles.js
@@ -3,12 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faPhone } from "@fortawesome/free-solid-svg-icons";
 import '../styles/Detalles.css';
 
-function Detalles({ closeModal }, props) {
+function Detalles({ closeModal, titulo, descripcion, servicio, localidad, nombre, numero, fecha }) {
   return (
     <div className="modalBackground">
-      <div className="contenedor-modal">
+      <div className="contenedor-modal" id="detalles-publicacion">
         <div className="modal-header">
-          <p>{props.titulo}</p>
+          <p>{titulo}</p>
           <button id="cerrar-modal" onClick={() => closeModal(false)}>
             <FontAwesomeIcon icon={faXmark} id="close-icon"/>
           </button>
@@ -17,28 +17,28 @@ function Detalles({ closeModal }, props) {
         <div className="modal-body">
           <div className="contenedor-detalles">
             <div className="detalles-content">
-              <p>{props.descripcion}</p>
+              <p>{descripcion}</p>
             </div>
             <div className="detalles-content">
-              <p className="detalles-servicio"><span>Servicio:</span>{props.servicio}</p>
-              <p className="detalles-localidad"><span>Localidad:</span>{props.localidad}</p>
+              <p className="detalles-servicio"><span>Servicio: </span>{servicio}</p>
+              <p className="detalles-localidad"><span>Localidad: </span>{localidad}</p>
             </div>
           </div>
           <div className="contenedor-detalles-perfil">
             <div className="perfil-foto">
               <img alt="foto"/>
             </div>
-            <p className="perfil-nombre">{props.nombre}</p>
+            <p className="perfil-nombre">{nombre}</p>
             <div className="perfil-numero">
               <FontAwesomeIcon icon={faPhone} />
-              <p>{props.numero}</p>
+              <p>{numero}</p>
             </div>
             <a href="#">Ir al perfil</a>
           </div>
         </div>
 
         <div className="modal-footer">
-            <p id="fecha">{props.fecha}</p>
+            <p id="fecha-publicacion">{fecha}</p>
         </div>
       </div>
     </div>
