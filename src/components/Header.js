3701560b4d65fde8a141fdeea4a07c9a12b5bd59
Juan Etcheverry <juanietcheverry8@gmail.com>
2023-09-29 20:20:34 -0300

    Se hicieron las peticiones para completar los campos select de los filtros

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,114 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/Header.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "../styles/Header.css";
+import axios from "axios";
 
 function Header({ onAbrirModal }) {
-    const [servicios, setServicios] = useState([]);
-    const [provincias, setProvincias] = useState([]);
-    const [localidades, setLocalidades] = useState([]);
-    const [selectedProvincia, setSelectedProvincia] = useState(""); 
-    const token = localStorage.getItem("token");
-    const config = { headers: { Authorization: `Bearer ${token}` } };
+  const [servicios, setServicios] = useState([]);
+  const [provincias, setProvincias] = useState([]);
+  const [localidades, setLocalidades] = useState([]);
+  const [provinciaSeleccionada, setProvinciaSeleccionada] = useState("");
+  const token = localStorage.getItem("token");
+  const config = { headers: { Authorization: `Bearer ${token}` } };
 
-    useEffect(() => {
-        axios.get(`http://localhost:3000/api/servicio`, config)
-            .then((response) => {
-                setServicios(response.data.servicios);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-        axios.get(`http://localhost:3000/api/provincia`, config)
-            .then((response) => {
-                setProvincias(response.data.provincias);
-                console.log("estado:", provincias);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/servicio`, config)
+      .then((response) => {
+        setServicios(response.data.servicios);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    axios
+      .get(`http://localhost:3000/api/provincia`, config)
+      .then((response) => {
+        setProvincias(response.data.provincias);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    }, []);
-    // --------------------------------------------------------------------------------------------
-    // Cambios en el select de provincias 1
-    // const handleProvinciaChange = (event) => {
-    //     const provinciaSeleccionada = event.target.value;
-    //     setSelectedProvincia(provinciaSeleccionada);
+  const handleProvinciaChange = (event) => {
+    const nuevaProvinciaSeleccionada = event.target.value;
+    setProvinciaSeleccionada(nuevaProvinciaSeleccionada);
+    console.log("seleccionada: ", nuevaProvinciaSeleccionada);
 
-    //     axios.post(`http://localhost:3000/api/localidadesxprovincia/${provinciaSeleccionada}`, config)
-    //         .then((response) => {
-    //             setLocalidades(response.data); 
-    //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //         });
-    // };
-    // Cambios en el select de provincias 2
-    const handleProvinciaChange = (event) => {
-        const provinciaSeleccionada = event.target.value;
-        console.log("seleccionada: ", provinciaSeleccionada);
+    axios
+      .get(
+        `http://localhost:3000/api/localidadesxprovincia/${nuevaProvinciaSeleccionada}`,
+        config
+      )
+      .then((response) => {
+        setLocalidades(response.data.localidades);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-        const provinciaData = {
-            nombreProvincia: provinciaSeleccionada
-        };
-        console.log("data: ", provinciaData);
-        axios.get(`http://localhost:3000/api/localidadesxprovincia?provinciaData=${JSON.stringify(provinciaData)}`, config)
-            .then((response) => {
-              setLocalidades(response.data); 
-              setLocalidades(response.data.localidades); 
-              console.log("response: ", response.data.localidades);
-              console.log("estado: ", localidades);
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-    };
-    // --------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-    return(
-        <header id="componente-header">
-            <div className="contenedor-publicacion">
-                <p>¿Estás buscando un servicio?</p>
-                <button className="publicacion" onClick={onAbrirModal}>Creá una publicación...</button>
-            </div>
-            <div className="linea-vertical"></div>
-            <div className="contenedor-busqueda">
-                <p>¿Estás buscando un trabajo?</p>
-                <form className="formulario-busqueda">
-                    
-                    <select name="Servicio">
-                        <option value="" disabled selected hidden>Servicio</option>
-                        {servicios.map((servicio) => (
-                            <option key={servicio.id} value={servicio.nombre}>
-                                {servicio.nombre}
-                            </option>
-                        ))}                    
-                    </select>
-                    <select name="Provincia" onChange={handleProvinciaChange} value={selectedProvincia}> 
-                        <option value="" disabled selected hidden>Provincia</option>
-                        {provincias.map((provincia) => (
-                            <option key={provincia.id} value={provincia.nombre}>
-                                {provincia.nombre}
-                            </option>
-                        ))} 
-                    </select>
-                    <select name="Localidad">
-                        <option value="" disabled selected hidden>Localidad</option>
-                        <option value="berazategui">Berazategui</option>
-                        <option value="berisso">Berisso</option>
-                        <option value="la plata">La Plata</option>
-                    </select>
-                    <button>Buscar</button>
-                </form>
-            </div>
-        </header>
-    );
-};
+  return (
+    <header id="componente-header">
+      <div className="contenedor-publicacion">
+        <p>¿Estás buscando un servicio?</p>
+        <button className="publicacion" onClick={onAbrirModal}>
+          Creá una publicación...
+        </button>
+      </div>
+      <div className="linea-vertical"></div>
+      <div className="contenedor-busqueda">
+        <p>¿Estás buscando un trabajo?</p>
+        <form className="formulario-busqueda">
+          <select name="Servicio">
+            <option value="" disabled selected hidden>
+              Servicio
+            </option>
+            {servicios.map((servicio) => (
+              <option key={servicio.id} value={servicio.nombre}>
+                {servicio.nombre}
+              </option>
+            ))}
+          </select>
+          <select
+            name="Provincia"
+            onChange={handleProvinciaChange}
+            value={provinciaSeleccionada}
+          >
+            <option value="" disabled selected hidden>
+              Provincia
+            </option>
+            {provincias.map((provincia) => (
+              <option key={provincia.id} value={provincia.nombre}>
+                {provincia.nombre}
+              </option>
+            ))}
+          </select>
+          <select name="Localidad">
+            <option value="" disabled selected hidden>
+              Localidad
+            </option>
+            {localidades.map((localidad) => (
+              <option key={localidad.id} value={localidad.nombre}>
+                {localidad.nombre}
+              </option>
+            ))}
+          </select>
+          <button>Buscar</button>
+        </form>
+      </div>
+    </header>
+  );
+}
 
 export default Header;
