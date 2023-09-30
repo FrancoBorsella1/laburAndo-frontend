79ff6a525bae80874c0898643145b4531579707b
FrancoBorsella1 <fborsella1@gmail.com>
2023-09-30 00:48:21 -0300

    feat: Se agregaron validaciones al registro

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -17,6 +17,16 @@ function Register() {
         idLocalidad: 1, //Por defecto, cambiar más adelante
     });
 
+    const [errores, seterrores] = useState({
+        nombre: '',
+        apellido: '',
+        email: '',
+        telefono: '',
+        clave: '',
+        usuario: '',
+        idLocalidad: '', 
+    });
+
     //Manejar cambios en los campos del formulario
     const handleInputChange = (event) => {
         const { name, value } = event.target;
@@ -29,6 +39,68 @@ function Register() {
     //Manejar envio del formulario
     const agregarUsuario = async (event) => {
         event.preventDefault();
+
+        //Validaciones
+        let erroresEncontrados = false;
+        const nuevosErrores = {
+            nombre: '',
+            apellido: '',
+            email: '',
+            telefono: '',
+            clave: '',
+            usuario: '',
+        };
+
+        //Validar que el nombre no esté vacío
+        if (!usuario.nombre) {
+            nuevosErrores.nombre = 'El nombre es obligatorio';
+            erroresEncontrados = true;
+        }
+
+        //Validar que el apellido no esté vacío
+        if (!usuario.apellido) {
+            nuevosErrores.apellido = 'El apellido es obligatorio';
+            erroresEncontrados = true;
+        }
+
+        //Validar que el correo electrónico no esté vacío y tenga el formato adecuado
+        if (!usuario.email) {
+            nuevosErrores.email = 'El correo electrónico es obligatorio';
+        }
+        
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!usuario.email.match(emailRegex)) {
+            nuevosErrores.email = 'Ingrese un correo electrónico válido';
+            erroresEncontrados = true;
+        }
+
+        //Validar que el telefono no esté vacío y no hayan caracteres
+        if (!usuario.telefono) {
+            nuevosErrores.telefono = 'El teléfono es obligatorio';
+            erroresEncontrados = true;
+        }
+
+        if (isNaN(usuario.telefono)) {
+            nuevosErrores.telefono = 'El teléfono debe tener un valor numérico';
+            erroresEncontrados = true;
+        }
+
+        //Validar que la contraseña tenga al menos 4 caracteres
+        if (usuario.clave.length < 4) {
+            nuevosErrores.clave = 'La contraseña debe tener al menos 4 caracteres';
+            erroresEncontrados = true;
+        }
+
+        //Validar que el usuario no esté vacío
+        if (!usuario.usuario) {
+            nuevosErrores.usuario = 'El usuario es obligatorio'
+            erroresEncontrados = true;
+        }
+
+        if (erroresEncontrados) {
+            seterrores(nuevosErrores);
+            return; //Si hay errores no se envía el formulario
+        }
 
         try{
             //Realizar la solicitud POST al servidor
@@ -59,48 +131,54 @@ function Register() {
                             placeholder='Nombre'
                             value={usuario.nombre}
                             onChange={handleInputChange}
-                            required
+                            
                         />
+                        {errores.nombre && <p className='error'>{errores.nombre}</p>}
                         <input 
                             type='text' 
                             name="apellido" 
                             placeholder='Apellido'
                             value={usuario.apellido}
                             onChange={handleInputChange}
-                            required
+                            
                         />
+                        {errores.apellido && <p className='error'>{errores.apellido}</p>}
                         <input 
                             type='text' 
                             name="email" 
                             placeholder='Correo electrónico'
                             value={usuario.email}
                             onChange={handleInputChange}
-                            required
+                            
                         />
+                        {errores.email && <p className='error'>{errores.email}</p>}
                         <input 
                             type='text' 
                             name="usuario" 
                             placeholder='Nombre de usuario'
                             value={usuario.usuario}
                             onChange={handleInputChange}
-                            required
+                            
                         />
+                        {errores.usuario && <p className='error'>{errores.usuario}</p>}
                         <input 
                             type='text' 
                             name="telefono" 
                             placeholder='Telefono'
                             value={usuario.telefono}
                             onChange={handleInputChange}
-                            required
+                            
                         />
+                        {errores.telefono && <p className='error'>{errores.telefono}</p>}
                         <input 
                             type='password' 
                             name="clave" 
                             placeholder='Contraseña'
                             value={usuario.clave}
                             onChange={handleInputChange}
-                            required
+                            
                         />
+                        {errores.clave && <p className='error'>{errores.clave}</p>}
                         {/* <input 
                             type='password' 
                             name="rep-password" 
