eaf5dda2d8a1bfbf62e7042b38b934df6162b369
cintiavalero <cintiavalero2001@gmail.com>
2023-10-19 18:26:59 -0300

    Se hizo la petición para traer las solicitudes de reseña pendientes

--- src/components/SolicitudesResenia.js
+++ src/components/SolicitudesResenia.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import "../styles/SolicitudesResenia.css"; /*cambiar*/ 
 import "../styles/modal.css"
 import AltaResena from './AltaResena';
+import axios from 'axios';
 
 
-function SolicitudesResenia({ closeModal }) {
+function SolicitudesResenia({ closeModal, idCalificadorProp}) {
+    const [solicitudesRecuperadas, setSolicitudesRecuperadas] = useState([]);
+    useEffect(() => {
+        axios
+            .get(`http://200.58.106.151:3000/api/resenas?idCalificador=${idCalificadorProp}`)
+            .then((response) => {
+                setSolicitudesRecuperadas(response.data);
+            })
+            .catch((error) => {
+                console.error('Error al obtener datos: ', error);
+            })
+    }, []);
+    console.log('resenas recuperadass: ', solicitudesRecuperadas);
 
     // Manejo del modal: alta de reseña
     const [modalAltaResenia, setmodalAltaResenia] = useState(false);
@@ -28,16 +41,22 @@ function SolicitudesResenia({ closeModal }) {
             </div>
             <div className="modal-body" id="lista-solicitudes-body">
                 <ul>
-                    <li>
-                        <p>Franco Ezequiel Borsella - Electricista</p>
-                        <div className="lista-solicitudes-botonera">
-                            <button className="lista-solicitudes-boton" type="button" onClick={abrirModalAltaResenia}>Valorar</button>
-                            <button id="eliminar-solicitud">
-                                <FontAwesomeIcon icon={faTrashCan} id="trash-icon" />
-                            </button>
-                        </div>
-                    </li>
+                    {solicitudesRecuperadas.map((solicitud) => (
+                        <li>
+                            <p>
+                                {(solicitud.calificado.nombre).toUpperCase() + ' ' + (solicitud.calificado.apellido).toUpperCase() + ' - '}
+                                {solicitud.calificado.servicios && solicitud.calificado.servicios.length > 0  ? solicitud.calificado.servicios[0].nombre: 'Sin servicio'}
+                            </p>
+                            <div className="lista-solicitudes-botonera">
+                                <button className="lista-solicitudes-boton" type="button" onClick={abrirModalAltaResenia}>Valorar</button>
+                                <button id="eliminar-solicitud">
+                                    <FontAwesomeIcon icon={faTrashCan} id="trash-icon" />
+                                </button>
+                            </div>
+                        </li>
+                    ))}
                 </ul>
+
             </div>
         </div>
         {modalAltaResenia && (<AltaResena closeModal={cerrarModalAltaResenia}/>)}
